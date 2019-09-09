d2a8a516efbe1aa82309116d3d67a70564f9895c
wen <steven.shen@aiyuangong.com>
2019-09-09 18:03:09 +0800

    js

---
+++ src/常见的js问题.md
@@ -0,0 +1,3 @@
+### 常见的js问题
+
+<img src="../assets/image/常见的js问题.png" />
---
+++ src/数组去重/index.js
@@ -0,0 +1,22 @@
+// set方法
+function uniquearray(array) { 
+  let unique_array= Array.from(new Set(array)) 
+  return unique_array
+}
+// 使用filter
+const unque_arry = (arr) => {
+  let unique_arry = arr.filter((elem, index, self) => {
+    return index = self.indexOf(elem)
+  })
+  return unique_arry
+}
+// 使用for循环
+const dups_names = ['Ron', 'Pal', 'Fred', 'Rongo', 'Ron']
+function dups_array(dups_names) {
+let unique = {};
+names.forEach(function(i) {
+    If (!unique[i]) {
+      unique[i] = true   }
+  });
+return Object.keys(unique)}   // Ron, Pal, Fred, Rongo
+Dups_array(names)
